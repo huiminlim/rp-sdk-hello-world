1d6009d02fe6c036d29e99af6febd1af5d18ffee
huiminlim <e0036379@u.nus.edu>
2024-03-20 23:35:25 +0800

    add blinks

--- main.c
+++ main.c
@@ -9,8 +9,27 @@
 
 int main() {
     stdio_init_all();
+
+    // Init GPIO if it exists
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+#endif
+
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+#endif
     }
 }
